3b1c39c0db83f3d63eb10ef1aadae5108ad1c84a
Vishvendra Singh <vish.gecj@gmail.com>
2018-08-16 18:42:47 +0530

    added pojo for reviews and trailers

---
+++ app/src/main/java/com/popmovies/model/ReviewModel.java
@@ -0,0 +1,82 @@
+package com.popmovies.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ReviewModel implements Parcelable{
+    private String id;
+    private String author;
+    private String content;
+    private String url;
+
+    public ReviewModel(String id, String author, String content, String url) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+        this.url = url;
+    }
+
+    protected ReviewModel(Parcel in) {
+        id = in.readString();
+        author = in.readString();
+        content = in.readString();
+        url = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(author);
+        dest.writeString(content);
+        dest.writeString(url);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<ReviewModel> CREATOR = new Creator<ReviewModel>() {
+        @Override
+        public ReviewModel createFromParcel(Parcel in) {
+            return new ReviewModel(in);
+        }
+
+        @Override
+        public ReviewModel[] newArray(int size) {
+            return new ReviewModel[size];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/main/java/com/popmovies/model/TrailerModel.java
@@ -0,0 +1,94 @@
+package com.popmovies.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TrailerModel implements Parcelable{
+    private String id;
+    private String key;
+    private String name;
+    private String site;
+    private String type;
+
+    public TrailerModel(String id, String key, String name, String site, String type) {
+        this.id = id;
+        this.key = key;
+        this.name = name;
+        this.site = site;
+        this.type = type;
+    }
+
+    protected TrailerModel(Parcel in) {
+        id = in.readString();
+        key = in.readString();
+        name = in.readString();
+        site = in.readString();
+        type = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(key);
+        dest.writeString(name);
+        dest.writeString(site);
+        dest.writeString(type);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<TrailerModel> CREATOR = new Creator<TrailerModel>() {
+        @Override
+        public TrailerModel createFromParcel(Parcel in) {
+            return new TrailerModel(in);
+        }
+
+        @Override
+        public TrailerModel[] newArray(int size) {
+            return new TrailerModel[size];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
