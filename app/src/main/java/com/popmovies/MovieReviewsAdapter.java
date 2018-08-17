3c8488d075162db4a6d08cc2c3c90d1f80f6a095
Vishvendra Singh <vish.gecj@gmail.com>
2018-08-17 14:48:57 +0530

    Added textviews for review item ui

--- app/src/main/java/com/popmovies/MovieReviewsAdapter.java
+++ app/src/main/java/com/popmovies/MovieReviewsAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.popmovies.model.ReviewModel;
 
@@ -28,7 +29,9 @@ public class MovieReviewsAdapter extends RecyclerView.Adapter<MovieReviewsAdapte
 
     @Override
     public void onBindViewHolder(@NonNull MovieReviewHolder holder, int position) {
-
+        ReviewModel review = mReviews[position];
+        holder.tvReviewAuthor.setText(review.getAuthor());
+        holder.tvReviewContent.setText(review.getContent());
     }
 
     @Override
@@ -41,9 +44,13 @@ public class MovieReviewsAdapter extends RecyclerView.Adapter<MovieReviewsAdapte
     }
 
     public static final class MovieReviewHolder extends RecyclerView.ViewHolder {
+        public TextView tvReviewContent;
+        private TextView tvReviewAuthor;
 
         public MovieReviewHolder(View itemView) {
             super(itemView);
+            tvReviewContent = (TextView) itemView.findViewById(R.id.tv_review_content);
+            tvReviewAuthor = (TextView) itemView.findViewById(R.id.tv_review_user_name);
         }
     }
 }
